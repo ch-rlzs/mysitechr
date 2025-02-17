b74eb663454a4fc4290e22c62a4a09d9b285b2af
chrlzs <chrlzs.chizl@gmail.com>
2025-02-17 17:37:21 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleSection(id) {
-    var section = document.getElementById(id);
-    if (section.style.display === "block") {
-        section.style.display = "none";
-    } else {
-        section.style.display = "block";
-    }
-}
