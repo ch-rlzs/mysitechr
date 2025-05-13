56b749073abf63add21c1b004ea1eaa1a2e20f0e
chrlzs <chrlzs.chizl@gmail.com>
2025-05-13 07:59:21 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgFYlZc9PI95uSEC_iejXEZOerZ8ebO44",
+  authDomain: "chrlzs-chat-645cf.firebaseapp.com",
+  databaseURL: "https://chrlzs-chat-645cf-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "chrlzs-chat-645cf",
+  storageBucket: "chrlzs-chat-645cf.appspot.com",
+  messagingSenderId: "426878855113",
+  appId: "1:426878855113:web:166da584829cffd5106a18",
+  measurementId: "G-5XW702X3TQ"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+// Export database reference
+export { db };
