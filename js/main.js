22d6fa02b68dc57ff1908f7cd09734172756bc82
chrlzs <chrlzs.chizl@gmail.com>
2025-05-13 20:08:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
-import { initAuth, cleanupAuth } from './auth.js';
+import { initAuth } from './auth.js';
 import { initChat } from './chat.js';
 
+// Initialize the application
 document.addEventListener('DOMContentLoaded', () => {
   initAuth();
   initChat();
-});
-
-// Cleanup when leaving page
-window.addEventListener('beforeunload', () => {
-  cleanupAuth();
 });
